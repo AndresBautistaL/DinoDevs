6864d635ae25fb8f318409085f372aab96a5bd49
Andrés <jandresbl96@gmail.com>
2022-08-29 00:37:47 -0500

    Ya no por favor :c

--- assets/js/clientes.js
+++
@@ -1,4 +0,0 @@
-const btnToggle = document.querySelector(".toggle-btn");
-btnToggle.addEventListener('click', function(){
-    document.getElementById('sidebar-container').classList.toggle('active');
-});
