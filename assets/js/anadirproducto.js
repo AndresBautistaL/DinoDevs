a517731cbef7e6875bd1fc9245eaa403389c62c1
Kim0i <belazki@hotmail.com>
2022-09-02 15:24:52 -0500

    Corrección Deploy

---
+++ assets/js/anadirproducto.js
@@ -0,0 +1,20 @@
+/* Validacion formulario */
+/* Traer inputs del formulario */
+
+const d = document;
+const Nombre = d.getElementById("Nombre");
+const Colores = d.getElementById("Colores");
+const Tallas = d.getElementById("Tallas");
+const Cantidad = d.getElementById("Cantidad");
+const Precio = d.getElementById("Precio");
+
+const subirRopa = ("Nombre","Colores","Tallas","Cantidad","Precio");
+console.log(subirRopa);
+
+/* const boton = d.getElementsByClassName(".btn");
+
+boton.addEventListener("click", function () {
+  
+
+}
+) */
